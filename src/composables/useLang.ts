ec4983e667d24779e1d8c423d7175964f9d76b77
Gabriel <gabrielmonteiroduete@gmail.com>
2026-04-28 22:34:01 -0300

    chore: add initial configs

---
+++ src/composables/useLang.ts
@@ -0,0 +1,15 @@
+import { useI18n } from 'vue-i18n'
+
+const useLang = () => {
+  const { locale } = useI18n()
+
+  const lang = locale
+
+  const setLang = (l: 'pt' | 'en') => {
+    locale.value = l
+  }
+
+  return { lang, setLang }
+}
+
+export default useLang
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
-import './style.css'
+import { createI18n } from 'vue-i18n'
+import { MotionPlugin } from '@vueuse/motion'
 import App from './App.vue'
+import './assets/main.css'
 
-createApp(App).mount('#app')
+import pt from './locales/pt-br.json'
+import en from './locales/en.json'
+
+const i18n = createI18n({
+  legacy: false,
+  locale: 'pt',
+  fallbackLocale: 'en',
+  messages: { pt, en },
+})
+
+createApp(App).use(i18n).use(MotionPlugin).mount('#app')
