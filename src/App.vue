fdbcc1c38e81b751afe2bf600d5f2ac3b77a36fa
Gabriel <gabrielmonteiroduete@gmail.com>
2026-06-05 17:58:02 -0300

    chore: configure prettier

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,7 @@
 <script setup lang="ts"></script>
 
-<template></template>
+<template>
+  <h1>teste</h1>
+  <p>asodaok</p>
+  <p>pasdfopdfkpdkadfkopopsfdkpoafsd sdkopf s dkopfsd opfsdopk fopsdkf opds</p>
+</template>
