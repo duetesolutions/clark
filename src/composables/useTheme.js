f2455fb9926a67188240ca310fa970487b96cd34
Gabriel <gabrielmonteiroduete@gmail.com>
2026-06-06 17:18:39 -0300

    feat: remove unnecessary files

--- src/composables/useTheme.js
+++
@@ -1,9 +0,0 @@
-import { useDark } from '@vueuse/core';
-const useTheme = () => {
-    const isDark = useDark();
-    const toggleTheme = () => {
-        isDark.value = !isDark.value;
-    };
-    return { isDark, toggleTheme };
-};
-export default useTheme;
